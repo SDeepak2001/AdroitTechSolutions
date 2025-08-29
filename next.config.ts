b3e4d9fa6985632e1e2cff5c292317783baf5a7b
SDeepak2001 <deepaksarupuri@gmail.com>
2025-08-29 13:44:28 -0700

    Third Final Fixes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",          // writes to ./out
+  output: "export",          // writes static site to ./out
   images: { unoptimized: true },
-  trailingSlash: true,       // cleaner static paths
+  trailingSlash: true
 };
 
 export default nextConfig;
